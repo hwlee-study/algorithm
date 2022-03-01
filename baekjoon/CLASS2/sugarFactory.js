1e31afb6655750503de0320e3837de61977c35cb
hwlee-study <hwlee8042@gmail.com>
2022-03-01 22:42:40 +0900

    solve one-day basic algorithm

---
+++ baekjoon/CLASS2/sugarFactory.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var input = fs
+  .readFileSync(`${__dirname}/TestCase/sugarFactory`)
+  .toString()
+  .trim()
+  .split("\n");
+
+let N = Number(input);
+let five = 0;
+let three = 0;
+
+while (N > 0) {
+  if (N % 5 === 0) {
+  }
+}
